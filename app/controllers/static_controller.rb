b31e92c5d81a715efdeb2818f2b7a87fb92e2688
cribbles <yaphetf@gmail.com>
2015-05-21 16:20:34 +0200

    Add a Static Pages controller, application layout, and sassified CSS spreadsheet

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,10 @@
+class StaticController < ApplicationController
+  def terms
+  end
+
+  def faq
+  end
+
+  def stats
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  root "application#hello"
+  get 'static/terms'
+
+  get 'static/faq'
+
+  get 'static/stats'
+
+  root           'application#hello'
+  get 'terms' => 'static#terms'
+  get 'faq'   => 'static#faq'
+  get 'stats' => 'static#stats'
 end
