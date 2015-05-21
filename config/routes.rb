7987c06f3a00abcffaf0df1e66c5b246b76f5c62
cribbles <yaphetf@gmail.com>
2015-05-21 17:10:30 +0200

    Fix routing issue in terms, create static pages header/footer, complete FAQ page, create stats placeholder

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'static/terms'
-
-  get 'static/faq'
-
-  get 'static/stats'
-
   root           'application#hello'
   get 'terms' => 'static#terms'
   get 'faq'   => 'static#faq'
