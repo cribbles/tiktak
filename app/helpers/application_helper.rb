ae79231498dc4292647de18cea46c2f37ff1236b
cribbles <yaphetf@gmail.com>
2015-05-26 18:10:58 +0200

    Provide :form_first key to render forms correctly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,11 +5,11 @@ module ApplicationHelper
     @benchmark ||= Time.now
   end
 
-  def width_override width
+  def width_override(width)
     width.nil? ? nil : "width: #{width}px !important;"
   end
 
-  def full_title page_title
+  def full_title(page_title)
     base_title = "WesACB"
     page_title.empty? ? base_title : base_title + " - " + page_title
   end
