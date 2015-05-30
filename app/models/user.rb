d34a9deff2d4e8143bf0a6ca73b2d9ecef31d386
cribbles <yaphetf@gmail.com>
2015-05-30 16:04:42 +0200

    Add user fixtures and digest method with tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,12 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
   private
 
     def downcase_email
