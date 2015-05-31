ab5248d2ff1763262077d8e713c184d5d4954a3a
cribbles <yaphetf@gmail.com>
2015-05-31 22:40:51 +0200

    Add cookies handling and tests

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -28,6 +28,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    delete logout_path
     follow_redirect!
     assert_not flash.empty?
     assert_select "a[href=?]", login_path
@@ -35,5 +36,15 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", user_path(@user), count: 0
     get login_path
     assert flash.empty?
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_nil cookies['remember_token']
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '0')
+    assert_nil cookies['remember_token']
   end
 end
