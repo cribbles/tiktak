387c52dfce6cbd070c8ffecf7b14499f80fe1d42
cribbles <yaphetf@gmail.com>
2015-06-01 21:02:04 +0200

    Fix bug in routes helper

--- app/helpers/routes_helper.rb
+++ app/helpers/routes_helper.rb
@@ -2,11 +2,13 @@ module RoutesHelper
 
   def topics_show?
     return false unless defined? @topic.id
+    return false if @topic.id.nil?
     current_page?(topic_path(@topic))
   end
 
   def posts_new?
     return false unless defined? @topic.id
+    return false if @topic.id.nil?
     if defined? @quote.id
       current_page?(quote_path(@topic, @quote))
     else
