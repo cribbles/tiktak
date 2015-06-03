409747065d3be8cac93ba53d59048006e769ad60
cribbles <yaphetf@gmail.com>
2015-06-03 14:22:02 +0200

    Fix topics#index integration tests, tweak fixtures

--- test/integration/topics_index_test.rb
+++ test/integration/topics_index_test.rb
@@ -6,9 +6,11 @@ class TopicsIndexTest < ActionDispatch::IntegrationTest
     get topics_path
     assert_template 'topics/index'
     assert_select 'div.pagination'
-    first_page_of_topics = Topic.paginate(page: 1)
+    first_page_of_topics = Topic.order(last_posted: :desc)
+                                .paginate(page: 1, per_page: 20)
     first_page_of_topics.each do |topic|
       assert_select 'a[href=?]', topic_path(topic), text: topic.title
+      assert_match topic.posts.first.content, response.body
     end
   end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -20,10 +20,11 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get login_path
     post login_path, session: { email: @user.email, password: 'password' }
     assert is_logged_in?
-    assert_redirected_to @user
+    assert_redirected_to profile_url
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
+#    assert_select "a[href=?]", user_path(@user)
     assert_select "a[href=?]", logout_path
     delete logout_path
     assert_not is_logged_in?
@@ -32,8 +33,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_not flash.empty?
     assert_select "a[href=?]", login_path
-    assert_select "a[href=?]", logout_path,      count: 0
-    assert_select "a[href=?]", user_path(@user), count: 0
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", profile_url, count: 0
     get login_path
     assert flash.empty?
   end
