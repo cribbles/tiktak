4aca46e67e2a7cc038c443871ead6b6a4add1afd
cribbles <yaphetf@gmail.com>
2015-06-04 19:13:03 -0400

    Add login integration test for unactivated users

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -3,13 +3,25 @@ require 'test_helper'
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = users(:fred)
+    @user             = users(:fred)
+    @unactivated_user = users(:wilma)
   end
 
   test "login with invalid information" do
     get login_path
     assert_template 'sessions/new'
     post login_path, session: { email: "", password: "" }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get login_path
+    assert flash.empty?
+  end
+
+  test "login with valid information as unactivated user" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, session: { email: @unactivated_user.email,
+                                password: 'password' }
     assert_template 'sessions/new'
     assert_not flash.empty?
     get login_path
