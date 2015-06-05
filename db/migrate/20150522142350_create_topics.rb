d8806d5aef17d0cfe7d36f4b89d6604e9aecf1d4
cribbles <yaphetf@gmail.com>
2015-06-05 15:54:24 -0400

    Allow topics.last_posted to be NULL

--- db/migrate/20150522142350_create_topics.rb
+++ db/migrate/20150522142350_create_topics.rb
@@ -3,7 +3,7 @@ class CreateTopics < ActiveRecord::Migration
     create_table :topics do |t|
       t.text :title
       t.timestamps null: false
-      t.datetime :last_posted, null: false
+      t.datetime :last_posted
     end
   end
 end
