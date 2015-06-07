a46553cbf71ebc03957d086e75984cd6a7193270
cribbles <yaphetf@gmail.com>
2015-06-07 17:26:13 -0400

    Remove title from PmTopics schema

--- db/migrate/20150605150512_create_pm_topics.rb
+++ db/migrate/20150605150512_create_pm_topics.rb
@@ -1,7 +1,6 @@
 class CreatePmTopics < ActiveRecord::Migration
   def change
     create_table :pm_topics do |t|
-      t.string :title
       t.datetime :last_posted
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 20150607013037) do
   add_index "pm_posts", ["user_id"], name: "index_pm_posts_on_user_id"
 
   create_table "pm_topics", force: :cascade do |t|
-    t.string   "title"
     t.datetime "last_posted"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
