98f894e0e0483c76e21e68cee3fc630842395716
cribbles <yaphetf@gmail.com>
2015-06-22 14:47:48 -0400

    Refactor PmPosts controller

--- app/controllers/pm_posts_controller.rb
+++ app/controllers/pm_posts_controller.rb
@@ -3,18 +3,18 @@ class PmPostsController < ApplicationController
   before_action :ensure_valid_user
 
   def create
-    @pm_topic = PmTopic.find_by(id: pm_post_params[:pm_topic_id])
-    @pm_post  = @pm_topic.pm_posts.build(pm_post_params)
+    @pm_post = pm_topic.pm_posts.build(pm_post_params)
     if @pm_post.save
-      @pm_topic.update_attributes(last_posted: @pm_post.created_at)
+      pm_topic.update_attributes(last_posted: @pm_post.created_at)
       @pm_post.update_attributes(ip_address: request.remote_ip,
                                  user_id:    current_user.id)
-      @pm_topic.update_attributes(user_handshake => true) if handshake_sent
-      redirect_to pm_topic_path(@pm_topic.id, anchor: "p" + @pm_post.id.to_s)
+      pm_topic.update_attributes(user_handshake => true) if handshake_sent
+      redirect_to pm_topic_path(pm_topic.id, anchor: "p" + @pm_post.id.to_s)
     else
-      @pm_posts = @pm_topic.pm_posts.order(created_at: :asc)
+      @pm_topic = pm_topic
+      @pm_posts = pm_topic.pm_posts.order(created_at: :asc)
       @user_handshake = user_handshake.to_s
-      @user_sent_handshake = @pm_topic.send(user_handshake)
+      @user_sent_handshake = pm_topic.send(user_handshake)
       render 'pm_topics/show'
     end
   end
@@ -25,8 +25,11 @@ class PmPostsController < ApplicationController
       params.require(:pm_post).permit(:pm_topic_id, :content, :handshake_sent)
     end
 
+    def pm_topic
+      PmTopic.find_by(id: pm_post_params[:pm_topic_id])
+    end
+
     def ensure_valid_user
-      pm_topic = PmTopic.find_by(id: pm_post_params[:pm_topic_id])
       redirect_to root_url unless pm_topic.valid_users.include?(current_user.id)
     end
 
@@ -35,7 +38,6 @@ class PmPostsController < ApplicationController
     end
 
     def user_handshake
-      pm_topic = PmTopic.find_by(id: pm_post_params[:pm_topic_id])
       if pm_topic.sender_id == current_user.id
         :sender_handshake
       elsif pm_topic.recipient_id = current_user.id
