fd39109f8afdf3111efb93d82fcc8078f2d425e4
cribbles <yaphetf@gmail.com>
2015-07-06 11:25:15 -0400

    Make rspec sessions tests compatible with ip caching methods

--- spec/factories/ip_caches.rb
+++ spec/factories/ip_caches.rb
@@ -1,16 +1,5 @@
 FactoryGirl.define do
   factory :ip_cache do
     ip_address '1.2.3.4'
-
-    trait :hellbanned do
-      hellbanned true
-    end
-
-    trait :blacklisted do
-      blacklisted false
-    end
-
-    factory :hellbanned_ip,  traits: :hellbanned
-    factory :blacklisted_ip, traits: :blacklisted
   end
 end
--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -2,12 +2,24 @@ require 'rails_helper'
 
 describe 'the login process', type: :feature do
 
+  before(:each) do
+    FactoryGirl.create(:ip_cache, ip_address: '1.2.3.4')
+  end
+
   let!(:new_user) do
     FactoryGirl.create(:user, email: 'new_user@example.com')
   end
 
   let!(:activated_user) do
     FactoryGirl.create(:activated_user, email: 'activated_user@example.com')
+  end
+
+  it 'logs in an activated user' do
+    visit '/login'
+    fill_in 'E-mail', with: 'activated_user@example.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log In'
+    expect(current_path).to eq '/'
   end
 
   it 'does not log in an unactivated user' do
