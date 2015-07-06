b8320fae872aac87158703ac6c3dd378323c3fa2
cribbles <yaphetf@gmail.com>
2015-07-06 12:05:14 -0400

    Switch to using TikTak branding

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_epsilon_session'
+Rails.application.config.session_store :cookie_store, key: '_tiktak_session'
--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
-    assert_equal full_title,        "WesACB"
-    assert_equal full_title('FAQ'), "WesACB - FAQ"
+    assert_equal full_title,        Settings.site_name
+    assert_equal full_title('FAQ'), Settings.site_name + " - FAQ"
   end
 end
