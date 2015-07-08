fce5d112c3f57c7845e8a2decf9427fc48cd5b93
cribbles <yaphetf@gmail.com>
2015-07-08 16:01:33 -0400

    Add rspec logout and invalid login tests

--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'the login process', type: :feature do
+describe 'the login/logout process', type: :feature do
 
   before(:each) do
     FactoryGirl.create(:ip_cache, ip_address: '1.2.3.4')
@@ -14,20 +14,34 @@ describe 'the login process', type: :feature do
     FactoryGirl.create(:activated_user, email: 'activated_user@example.com')
   end
 
-  it 'logs in an activated user' do
+  it 'does not log in user with invalid information' do
     visit '/login'
-    fill_in 'E-mail', with: 'activated_user@example.com'
-    fill_in 'Password', with: 'password'
+    fill_in 'E-mail', with: ''
+    fill_in 'Password', with: ''
     click_button 'Log In'
-    expect(current_path).to eq '/'
+    expect(current_path).to eq '/login'
+    expect(page).to have_selector '.alert'
   end
 
-  it 'does not log in an unactivated user' do
+  it 'doe snor log in an unactivated user' do
     visit '/login'
     fill_in 'E-mail', with: 'new_user@example.com'
     fill_in 'Password', with: 'password'
     click_button 'Log In'
     expect(current_path).to eq '/login'
     expect(page).to have_selector '.alert'
+  end
+
+  it 'logs in from root_path as an activated user followed by log out' do
+    visit '/'
+    click_link 'Log In'
+    expect(current_path).to eq '/login'
+    fill_in 'E-mail', with: 'activated_user@example.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log In'
+    expect(current_path).to eq '/'
+    click_link 'Log Out'
+    expect(current_path).to eq '/'
+    expect(page).not_to have_link 'Log Out'
   end
 end
