c5180e3d709205bdb2ede73b2a15dc2d4ceeec7c
cribbles <yaphetf@gmail.com>
2015-07-10 10:21:43 -0400

    Move top partial helper logic into CSS

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,11 +1,5 @@
 module TopicsHelper
 
-  def top_partial
-    @td_count ||= 0
-    @td_count += 1
-    "border-top: 7px solid #FFFFFF !important;" unless @td_count > 2
-  end
-
   def last_posted_for(topic)
     hellbanned? ? topic.last_posted_hb : topic.last_posted
   end
