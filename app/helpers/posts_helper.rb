9d773ea8a78254c3a0d0e3e9fdb7d40ee19f936c
cribbles <yaphetf@gmail.com>
2015-07-10 13:27:06 -0400

    Add helper methods for topic/post views, improve CSS-led display

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,17 @@
 module PostsHelper
 
+  def displayable?(post)
+    post.visible? || (hellbanned? && post.hellbanned)
+  end
+
+  def has_displayable_quote?(post)
+    post.quoted && displayable?(post.quoted)
+  end
+
+  def anchor_tag(post)
+    content_tag :a, nil, name: anchor_for(post)
+  end
+
   def poster_for(post, page: page)
     page   ||= 1
     @count ||= 0
@@ -13,12 +25,23 @@ module PostsHelper
     post_number
   end
 
-  def displayable?(post)
-    post.visible? || (hellbanned? && post.hellbanned)
+  def quote_link_for(post)
+    link_to 'Quote', quote_path(post.topic, post)
   end
 
-  def has_displayable_quote?(post)
-    post.quoted && displayable?(post.quoted)
+  def contact_link_for(topic, post: nil)
+    contact = 'contact'
+    contact.capitalize! if post
+    post ||= topic.posts.first
+    link_to contact, new_pm_topic_path(topic, post), class: 'contact'
+  end
+
+  def report_link_for(topic, post: nil)
+    row = post ? 'post' : 'topic'
+    post ||= topic.posts.first
+    link_to 'Report', topic_post_path(topic, post),
+      method: :patch,
+      data: { confirm: "Flag this #{row} for moderation?" }
   end
 
   def delete_link_for(topic, post: nil)
@@ -29,6 +52,17 @@ module PostsHelper
       path = topic
       msg = 'This action will remove the entire topic. Are you sure?'
     end
-    link_to 'X', path, class: 'delete', method: :delete, data: { confirm: msg }
+    link_to 'X', path,
+      method: :delete,
+      data: { confirm: msg },
+      class: 'delete'
+  end
+
+  def context_link_for(post)
+    link_to 'Context', topic_path_for(post)
+  end
+
+  def unflag_link_for(post)
+    link_to 'Unflag', dismiss_path(post), method: :patch, class: 'dismiss'
   end
 end
