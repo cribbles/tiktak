89f2f14a75c63ce815be015cb6f1e9fc452c83b3
cribbles <yaphetf@gmail.com>
2015-07-14 21:38:00 -0400

    Add rspec test for valid PM transaction

--- spec/features/pm_spec.rb
+++ spec/features/pm_spec.rb
@@ -10,9 +10,9 @@ describe 'the private messaging system', type: feature do
     FactoryGirl.create(:activated_user, email: 'sender@example.com')
   end
 
-#  let(:recipient) do
-#    FactoryGirl.create(:activated_user, email: 'recipient@example.com')
-#  end
+  let(:recipient) do
+    FactoryGirl.create(:activated_user, email: 'recipient@example.com')
+  end
 
   it 'disallows a user from sending themselves a private message' do
     visit '/login'
@@ -20,14 +20,37 @@ describe 'the private messaging system', type: feature do
     fill_in 'Password', with: 'password'
     click_button 'Log In'
     expect(current_path).to eq '/'
-    expect(page).to have_content 'sender@example.com'
     click_link 'New Topic'
     fill_in 'Title', with: 'Inspiring topic title'
     fill_in 'Message', with: 'Bold and motivational message body'
     click_button 'Post'
-    expect(current_path).to eq '/topics/1'
     click_link 'Contact'
     expect(current_path).to eq '/topics/1'
     expect(page).to have_css 'alert'
+  end
+
+  it 'allows a user to send another user a private message' do
+    visit '/login'
+    fill_in 'E-mail', with: 'recipient@example.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log In'
+    expect(current_path).to eq '/'
+    click_link 'New Topic'
+    fill_in 'Title', with: 'Inspiring topic title'
+    fill_in 'Message', with: 'Bold and motivational message body'
+    click_button 'Post'
+    click_button 'Log Out'
+    click_button 'Log In'
+    fill_in 'E-mail', with: 'sender@example.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log In'
+    click_link 'Contact'
+    expect(current_path).to eq '/topics/1/posts/1/contact'
+    fill_in 'Title', with: 'Hello, sender!'
+    fill_in 'Message', with: 'What\'s the haps?'
+    click_button 'Post'
+    expect(current_path).to eq '/private-messages/1'
+    expect(page).to have_content 'Hello, sender!'
+    expect(page).to have_content 'What\'s the haps?'
   end
 end
