3128bd496272e59cc12ae92220cb36b3960a6bf5
cribbles <yaphetf@gmail.com>
2015-07-23 20:46:34 -0400

    Add capybara spec for flagged posts

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -22,6 +22,10 @@ describe 'the moderation system', type: :feature do
     FactoryGirl.create(:post)
   end
 
+  let(:flagged_post) do
+    FactoryGirl.create(:post, flagged: true)
+  end
+
   it 'requires users to be logged in to report posts' do
     visit '/topics/1'
     click_link 'Report'
@@ -29,15 +33,21 @@ describe 'the moderation system', type: :feature do
   end
 
   it 'displays delete links for admin users only' do
-    login_as('admin@example.com')
+    login_as 'admin@example.com'
     visit '/'
     expect(page).to have_link 'X'
     visit '/topics/1'
     expect(page).to have_link 'X'
     log_out
-    login_as('user@example.com')
+    login_as 'user@example.com'
     expect(page).to_not have_link 'X'
     visit '/'
     expect(page).to_not have_link 'X'
+  end
+
+  it 'displays queue flash alert when logged in as an admin' do
+    login_as 'admin@example.com'
+    visit '/'
+    expect(page).to have_css 'alert'
   end
 end
