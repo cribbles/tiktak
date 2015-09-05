d9a4ca1879de37861f93e43bc4dbff1545008574
cribbles <yaphetf@gmail.com>
2015-09-05 11:36:11 -0400

    Improve association prefetching in Topic/Post controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,21 @@
 class PostsController < ApplicationController
   include TopicsLibrary
+
   before_action :ensure_topic_exists
   before_action :ensure_admin,            only: :destroy
   before_action :ensure_logged_in,        only: :update
-  before_action :ensure_quote_associated, only: [:new, :create] 
+  before_action :ensure_quote_associated, only: [:new, :create]
   before_action :ensure_post_exists,      only: [:update, :destroy]
 
   def new
     @topic = topic
-    @quote = post 
+    @quote = post
     @post  = @topic.posts.build
   end
 
   def create
     @post = topic.posts.build(post_params)
+
     if captcha_verified(@post) && @post.save
       topic.update_attributes(last_posted_hb: @post.created_at)
       topic.update_attributes(last_posted: @post.created_at) unless hellbanned?
@@ -21,13 +23,13 @@ class PostsController < ApplicationController
       @post.update_attributes(user_id: current_user.id) if logged_in?
       @post.hellban if hellbanned?
 
-      redirect_to topic_path_for(@post) 
+      redirect_to topic_path_for(@post)
     else
       render 'new'
     end
   end
 
-  def update 
+  def update
     post.flag unless hellbanned?
 
     flash[:info] = "Post has been marked for moderation. Thanks!"
@@ -43,25 +45,30 @@ class PostsController < ApplicationController
 
   private
 
-    def post_params
-      params.require(:post).permit(:topic_id, :content, :quote, :contact)
-    end
+  def post_params
+    params
+      .require(:post)
+      .permit(:topic_id, :content, :quote, :contact)
+  end
 
-    def topic
-      id = params[:topic_id] || post_params[:topic_id]
+  def topic
+    topic_id = params[:topic_id] || post_params[:topic_id]
 
-      Topic.find_by(displayable where: { id: id })
-    end
+    Topic
+      .joins(:posts)
+      .joins("LEFT OUTER JOIN users ON users.id = posts.user_id")
+      .find_by(displayable( where: { id: topic_id }))
+  end
 
-    def post
-      topic.posts.find_by(displayable where: { id: params[:id] })
-    end
+  def post
+    topic.posts.find_by(displayable(where: { id: params[:id] }))
+  end
 
-    def ensure_post_exists
-      redirect_to root_url unless post
-    end
+  def ensure_post_exists
+    redirect_to root_url if post.nil?
+  end
 
-    def ensure_quote_associated
-      redirect_to root_url if params[:id] && !post
-    end
+  def ensure_quote_associated
+    redirect_to root_url if params[:id] && post.nil?
+  end
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,12 +1,15 @@
 class TopicsController < ApplicationController
   include TopicsLibrary
-  before_action :ensure_admin, only: :destroy
+
+  before_action :ensure_admin,        only: :destroy
   before_action :ensure_topic_exists, only: [:show, :destroy]
   before_action :increment_views,     only: :show
 
   def index
     @topics = Topic.where(displayable)
-                   .order(order)
+                   .joins(:posts)
+                   .order(display_order)
+                   .group("topics.id")
                    .paginate(page: params[:page], per_page: 20)
   end
 
@@ -24,13 +27,17 @@ class TopicsController < ApplicationController
 
   def create
     @topic = Topic.new(topic_params)
+
     if captcha_verified(@topic) && @topic.save
       post = @topic.posts.first
 
       @topic.update_attributes(last_posted:    post.created_at,
                                last_posted_hb: post.created_at)
-      @topic.update_with_nested(user_id: current_user.id) if logged_in?
-      @topic.update_with_nested(hellbanned: true) if hellbanned?
+
+      [@topic, post].each do |table|
+        table.update_attributes(user_id: current_user.id) if logged_in?
+        table.update_attributes(hellbanned: true) if hellbanned?
+      end
 
       redirect_to @topic
     else
@@ -47,21 +54,26 @@ class TopicsController < ApplicationController
 
   private
 
-    def topic_params
-      params.require(:topic).
-        permit(:title, posts_attributes: [:content, :contact])
-    end
+  def topic_params
+    params
+      .require(:topic)
+      .permit(:title, posts_attributes: [:content, :contact])
+  end
 
-    def topic
-      Topic.find_by(displayable where: { id: params[:id] })
-    end
+  def topic
+    Topic
+      .joins(:posts)
+      .joins("LEFT OUTER JOIN users ON users.id = posts.user_id")
+      .find_by(displayable( where: { id: params[:id] }))
+  end
 
-    def increment_views
-      topic.increment_views
-    end
+  def increment_views
+    topic.increment_views
+  end
 
-    def order
-      last_posted = hellbanned? ? :last_posted_hb : :last_posted
-      { last_posted => :desc }
-    end
+  def display_order
+    last_posted = (hellbanned? ? :last_posted_hb : :last_posted)
+
+    { last_posted => :desc }
+  end
 end
